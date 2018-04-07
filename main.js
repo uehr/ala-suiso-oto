9b83369df0c5953b0f0eb738b19034a8a6e10261
yudai-uehara <yuudai_17n1100760@nnn.ed.jp>
2018-04-07 19:02:39 +0900

    Update: started log

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const twitter = require("twitter")
 const atoms = require("./atoms.json")
 const cron = require("node-cron")
+const moment = require("moment")
+const date_format = "YYYY-MM-DD hh:mm:ss"
 const tclient = new twitter({
   consumer_key: process.env.twitter_suiso_consumer_key,
   consumer_secret: process.env.twitter_suiso_consumer_secret,
@@ -8,10 +10,12 @@ const tclient = new twitter({
   access_token_secret: process.env.twitter_suiso_access_token_secret
 })
 
+console.log("started: " + moment().format(date_format))
+
 cron.schedule(`0 0 * * * * *`, () => {
   const random_atom = atoms[Math.floor(Math.random() * atoms.length)]
   const tweet_text = `あぁ～！ ${random_atom} の音ォ〜！！`
   tclient.post('statuses/update', {status: tweet_text}, (error, tweet, response) => {
     if(error) throw error;
-  });
+  })
 })

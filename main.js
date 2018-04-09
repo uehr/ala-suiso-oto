a41a0e0322c7ae692daa500e25da5fb09d956bd8
yudai-uehara <yuudai_17n1100760@nnn.ed.jp>
2018-04-09 17:41:59 +0900

    Change: nodemon to forever module

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ console.log(process.env.twitter_suiso_access_token_key)
 console.log(process.env.twitter_suiso_access_token_secret)
 console.log("started: " + moment().format(date_format))
 
-cron.schedule(`0 0 * * * * *`, () => {
+sron.schedule(`0 0 * * * * *`, () => {
   const random_atom = atoms[Math.floor(Math.random() * atoms.length)]
   const tweet_text = `あぁ～！ ${random_atom} の音ォ〜！！`
   tclient.post('statuses/update', {status: tweet_text}, (error, tweet, response) => {

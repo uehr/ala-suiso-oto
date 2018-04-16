d62236977ef48c7db4c2a56f57cdb43d2c1a0f9a
yudai-uehara <yuudai_17n1100760@nnn.ed.jp>
2018-04-16 22:38:21 +0900

    Add: push to heroku

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const twitter = require("twitter")
 const atoms = require("./atoms.json")
 const cron = require("node-cron")
 const moment = require("moment")
+const http = require("http")
 const date_format = "YYYY-MM-DD hh:mm:ss"
 const tclient = new twitter({
   consumer_key: process.env.twitter_suiso_consumer_key,
@@ -10,13 +11,15 @@ const tclient = new twitter({
   access_token_secret: process.env.twitter_suiso_access_token_secret
 })
 
-console.log(process.env.twitter_suiso_consumer_key)
-console.log(process.env.twitter_suiso_consumer_secret)
-console.log(process.env.twitter_suiso_access_token_key)
-console.log(process.env.twitter_suiso_access_token_secret)
 console.log("started: " + moment().format(date_format))
 
-sron.schedule(`0 0 * * * * *`, () => {
+//app running check for heroku
+http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text:plain"})
+  res.end(process.env.app_status)
+}).listen(process.env.PORT || 8080)
+
+cron.schedule(`0 0 * * * * *`, () => {
   const random_atom = atoms[Math.floor(Math.random() * atoms.length)]
   const tweet_text = `あぁ～！ ${random_atom} の音ォ〜！！`
   tclient.post('statuses/update', {status: tweet_text}, (error, tweet, response) => {
